046be09defba59ef9239331c84c0c86f5107b43a
Bayryam <bayryambashchoban@gmail.com>
2024-03-13 16:36:49 +0200

    add routing for themes sections

--- Components-Workshop/workshop-components/src/app/theme/theme-routing.module.ts
+++ Components-Workshop/workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { MainComponent } from "../main/main.component";
-import { NewThemeComponent } from "./new-theme/new-theme.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from '../main/main.component';
+import { NewThemeComponent } from './new-theme/new-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 const routes: Routes = [
-    {path: 'themes' , component:  MainComponent},
-    {path: 'add-theme' , component:  NewThemeComponent}];
+  {
+    path: 'themes',
+    children: [
+      { path: '', pathMatch: 'full', component: MainComponent },
+      { path: ':themeID', component: CurrentThemeComponent },
+    ],
+  },
+
+  { path: 'add-theme', component: NewThemeComponent },
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ThemeRoutingModule { }
+export class ThemeRoutingModule {}
